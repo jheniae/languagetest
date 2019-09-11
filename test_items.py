0358b43cfe56d47a4bd3f40698de1a7dea7c8702
jheniae <jheniae@gmail.com>
2019-09-11 13:36:07 +0300

    add test_items.py

---
+++ test_items.py
@@ -0,0 +1,9 @@
+link = 'http://selenium1py.pythonanywhere.com/es/catalogue/hackers-painters_185/'
+link2 = 'http://selenium1py.pythonanywhere.com/es/catalogue/hacking-exposed-non-fifth-edition_188/'
+def test_check_button_add_basket(browser):
+        browser.get(link)
+        button = browser.find_element_by_css_selector("#add_to_basket_form> .btn-add-to-basket")
+        try:
+            assert button
+        except AssertionError:
+            print("Button add to basket apsent")
