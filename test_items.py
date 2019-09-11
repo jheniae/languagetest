64c7592560754c5b35c2e7cf238605a58d524daa
jheniae <jheniae@gmail.com>
2019-09-11 16:02:27 +0300

    fix test_items

--- test_items.py
+++ test_items.py
@@ -1,5 +1,4 @@
-link = 'http://selenium1py.pythonanywhere.com/es/catalogue/hackers-painters_185/'
-link2 = 'http://selenium1py.pythonanywhere.com/es/catalogue/hacking-exposed-non-fifth-edition_188/'
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_check_button_add_basket(browser):
         browser.get(link)
         button = browser.find_element_by_css_selector("#add_to_basket_form> .btn-add-to-basket")
