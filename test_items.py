17484b9f410292ec8727f974cd4e4b520408c4c3
jheniae <jheniae@gmail.com>
2019-09-11 17:37:44 +0300

    fix test_items

--- test_items.py
+++ test_items.py
@@ -1,5 +1,7 @@
+import time
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_check_button_add_basket(browser):
         browser.get(link)
-        button = browser.find_element_by_css_selector("#add_to_basket_form> .btn-add-to-basket")
-        assert button
+        time.sleep(30)
+        button = browser.find_element_by_css_selector("#add_to_basket_form> .btn-add-to-basket").is_displayed()
+        assert button, "Button apsend"
